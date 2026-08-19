7a67aec884bd043e8d20728f391f721c592af80d
alawnbloodfighters-arch <alawnbloodfighters@gmail.com>
2026-08-19 09:14:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const form = document.getElementById("bloodRequestForm");
 const submitButton = document.getElementById("submitButton");
 
 const SCRIPT_URL =
-  "https://script.google.com/macros/s/AKfycbzeU2hPuL7B4cddMZsFdGp5zPPjLHOwrNX7fM5t1vmuwbP0APDIVY1s1dMoOsuGQC0gKA/exec";
+  "https://script.google.com/macros/s/AKfycbxJTlqPoaBGzIAbROQyeQfVzCJ5LLGv2aizbg5-Go9sbjao53sD95vD5sz7fsttRX2MBA/exec";
 
 
 form.addEventListener("submit", async function (event) {
